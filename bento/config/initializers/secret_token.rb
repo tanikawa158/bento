65e0564956dd3e701ae32fb98d4db006e9bb318a
Akihiko Tanikawa <tanikawa158@kanazawa-es.com>
2013-02-02 14:41:29 +0900

    First commit

---
+++ bento/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bento::Application.config.secret_token = '125e963031938826731bf4ee4720f5874eac22948b526b18a6d491f2c239aee31d30241f3385c598ae9b1ff5cf77e9a1e9d4992fb7814e36ac844a04143e8e34'
