47937c4c14c6947f8ab783c1f3ae1b1e3c567b4e
Akihiko Tanikawa <tanikawa158@kanazawa-es.com>
2013-02-25 20:18:27 +0900

    User.user_nameにvalidate追加
    user_nameに空白無許可、一意であること、英数字限定の検証を追加。

--- bento/app/models/user.rb
+++ bento/app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   belongs_to :role
   belongs_to :company
   validates_presence_of :password, :on => :create
+  validates :user_name, :presence => true, :uniqueness => true, :format => { :with => /[a-zA-Z0-9]+/i }
   attr_accessible :company_id, :password, :role_id, :user_name, :password_confirmation
 end
