d2ceb9943be3ffe3682d24bef794568361464e34
Akihiko Tanikawa <tanikawa158@kanazawa-es.com>
2013-03-09 12:43:47 +0900

    Order: 注文できない問題を修正

--- bento/app/models/order.rb
+++ bento/app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :item
-  attr_accessible :item_name, :item_price, :status, :user_name
+  attr_accessible :item_name, :item_price, :status, :user_name, :user_id, :item_id, :order_status
 end
